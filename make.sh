618552d068dfca36bab7ad0ce968944236d92384
hudoznik <hudoznik@hudoznik.stroymarket.local>
2022-05-16 11:44:52 +0300

    update

--- make.sh
+++ make.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd $1
 folder=${PWD##*/}
-v=`cat mod_km_proguct_groups.xml|grep '<version>'|awk -F '>' '{print $2}'|awk '{print $1}'`;
+v=`cat mod_km_proguct_groups.xml|grep '<version>'|awk -F '>' '{print $2}'|awk -F '<' '{print $1}'`;
 name=`echo ${folder/.v./.v.$v.}.zip`;
 rm $name;
-zip -r $name install.xml upload
+# zip -r $name install.xml upload
 echo Модуль $name;
 git add *
 git commit -m "update"
